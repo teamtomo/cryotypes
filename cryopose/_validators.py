2ebc8827c372f9943fd05ba9238f0313f006b3e9
alisterburt <alisterburt@gmail.com>
2022-06-14 18:28:29 +0100

    validator import

--- cryopose/_validators.py
+++ cryopose/_validators.py
@@ -1,24 +1,24 @@
 import pandas as pd
 
-from ._data_labels import CryoPoseDataLabels
+from ._data_labels import CryoPoseDataLabels as CPDL
 
 
 def validate_3d_cryopose_df(df: pd.DataFrame) -> bool:
     """Validate a cryopose dataframe for particle poses in 3D."""
-    required_columns = [
-        CryoPoseDataLabels.POSITION_X,
-        CryoPoseDataLabels.POSITION_Y,
-        CryoPoseDataLabels.POSITION_Z,
-        CryoPoseDataLabels.ORIENTATION,
+    required_columns_3d = [
+        CPDL.POSITION_X,
+        CPDL.POSITION_Y,
+        CPDL.POSITION_Z,
+        CPDL.ORIENTATION,
     ]
-    return all(k in df for k in required_columns)
+    return all(k in df for k in required_columns_3d)
 
 
 def validate_2d_cryopose_df(df: pd.DataFrame) -> bool:
     """Validate a cryopose dataframe for particle poses in 3D."""
-    required_columns = [
-        CryoPoseDataLabels.POSITION_X,
-        CryoPoseDataLabels.POSITION_Y,
-        CryoPoseDataLabels.ORIENTATION,
+    required_columns_2d = [
+        CPDL.POSITION_X,
+        CPDL.POSITION_Y,
+        CPDL.ORIENTATION,
     ]
-    return all(k in df for k in required_columns)
+    return all(k in df for k in required_columns_2d)
