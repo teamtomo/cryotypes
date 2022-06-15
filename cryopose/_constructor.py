524f0c29bfcb0ae4422817bc2669f1c58537619e
Lorenzo Gaifas <brisvag@gmail.com>
2022-06-15 17:52:02 +0200

    fix pre-commit, apply to everything

--- cryopose/_constructor.py
+++ cryopose/_constructor.py
@@ -1,14 +1,16 @@
-from typing import Mapping, Optional, Sequence, TypeVar, Union, Literal
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Mapping, Sequence
 
 import numpy as np
 import pandas as pd
 from scipy.spatial.transform import Rotation
 
 from ._data_labels import CryoPoseDataLabels as CPDL
-from ._validators import validate_positions, validate_orientations, validate_cryopose
+from ._validators import validate_cryopose, validate_orientations, validate_positions
 
-
-_T = TypeVar("_T")
+if TYPE_CHECKING:
+    import Literal
 
 
 def _construct_empty_cryopose_df(ndim: Literal[2, 3] = 3) -> pd.DataFrame:
@@ -24,10 +26,10 @@ def _construct_empty_cryopose_df(ndim: Literal[2, 3] = 3) -> pd.DataFrame:
 
 def construct_cryopose_df(
     positions: np.ndarray,
-    orientations: Optional[Rotation] = None,
-    experiment_ids: Optional[Union[str, Sequence[str]]] = None,
-    pixel_spacing_angstroms: Optional[Union[float, Sequence[float]]] = None,
-    metadata: Optional[Mapping[str, Sequence]] = None,
+    orientations: Rotation | None = None,
+    experiment_ids: str | Sequence[str] | None = None,
+    pixel_spacing_angstroms: float | Sequence[float] | None = None,
+    metadata: Mapping[str, Sequence] | None = None,
     ndim: Literal[2, 3] = 3,
 ) -> pd.DataFrame:
     """Constructor for a valid cryopose DataFrame."""
@@ -43,7 +45,7 @@ def construct_cryopose_df(
     df[CPDL.PIXEL_SPACING] = pixel_spacing_angstroms
 
     if experiment_ids is None:
-        experiment_ids = '0'
+        experiment_ids = "0"
     df[CPDL.EXPERIMENT_ID] = experiment_ids
 
     # optional columns
--- cryopose/_validators.py
+++ cryopose/_validators.py
@@ -1,24 +1,35 @@
-from typing import Union, Sequence, Literal
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Sequence
 
 import numpy as np
-from numpy.typing import ArrayLike, NDArray
 import pandas as pd
+from numpy.typing import ArrayLike, NDArray
 from pandas.api.types import is_numeric_dtype, is_string_dtype
 from scipy.spatial.transform import Rotation
 
 from ._data_labels import CryoPoseDataLabels as CPDL
 
+if TYPE_CHECKING:
+    import Literal
+
 
 def validate_positions(positions: ArrayLike, ndim: Literal[2, 3] = 3) -> NDArray:
     positions = np.asarray(positions, dtype=float)
     if positions.ndim != 2 or positions.shape[1] not in (2, 3):
-        raise ValueError(f'positions must be a (n, 2) or (n, 3) array, got {positions.shape}')
+        raise ValueError(
+            f"positions must be a (n, 2) or (n, 3) array, got {positions.shape}"
+        )
     if ndim != positions.shape[1]:
-        raise ValueError(f'positions are {positions.shape[1]}D, but a {ndim}D cryopose was requested')
+        raise ValueError(
+            f"positions are {positions.shape[1]}D, but a {ndim}D cryopose was requested"
+        )
     return positions
 
 
-def validate_orientations(orientations: Union[Rotation, Sequence[Rotation]], ndim: Literal[2, 3] = 3) -> Rotation:
+def validate_orientations(
+    orientations: Rotation | Sequence[Rotation], ndim: Literal[2, 3] = 3
+) -> Rotation:
     return Rotation.concatenate(orientations)
 
 
@@ -32,18 +43,24 @@ def validate_cryopose(df: pd.DataFrame, ndim: Literal[2, 3] = 3) -> pd.DataFrame
 
     if CPDL.ORIENTATION not in df:
         raise KeyError(CPDL.ORIENTATION)
-    # cannot just check dtype, which will just be object, so we have to validate the objects themselves
+    # cannot just check dtype, so we have to validate the objects themselves
     if len(df) > 0:
         validate_orientations(df[CPDL.ORIENTATION], ndim=ndim)
 
     if CPDL.PIXEL_SPACING not in df:
         raise KeyError(CPDL.PIXEL_SPACING)
     if not is_numeric_dtype(df[CPDL.PIXEL_SPACING]):
-        raise TypeError(f'dtype of "{CPDL.PIXEL_SPACING}" should be a Number, got {df[CPDL.PIXEL_SPACING].dtype}')
+        raise TypeError(
+            f'dtype of "{CPDL.PIXEL_SPACING}" should be a Number, '
+            f"got {df[CPDL.PIXEL_SPACING].dtype}"
+        )
 
     if CPDL.EXPERIMENT_ID not in df:
         raise KeyError(CPDL.EXPERIMENT_ID)
     if not is_string_dtype(df[CPDL.EXPERIMENT_ID]):
-        raise TypeError(f'dtype of "{CPDL.EXPERIMENT_ID}" should be a string, got {df[CPDL.EXPERIMENT_ID].dtype}')
+        raise TypeError(
+            f'dtype of "{CPDL.EXPERIMENT_ID}" should be a string, '
+            f"got {df[CPDL.EXPERIMENT_ID].dtype}"
+        )
 
     return df
